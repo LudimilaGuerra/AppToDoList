5c3e163502936c023be42b5422e9555965909d58
Luckas2005 <luckas.git@gmail.com>
2025-09-08 15:18:00 -0300

    Implementado case 4-(Excluir Tarefa)

---
+++ AppToDoList-main/README.md
@@ -0,0 +1,2 @@
+# AppToDoList
+Este exercício consiste em desenvolver um sistema de gerenciamento de tarefas (ToDoList) em Java utilizando conceitos de POO e CRUD com armazenamento em memória através de uma ArrayList.
---
+++ AppToDoList-main/src/main/java/ucb/aplicativo/service/TarefaService.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ucb.aplicativo.service;
+
+import java.util.List;
+import ucb.aplicativo.model.Tarefas;
+import java.util.ArrayList;
+/**
+ *
+ * @author ludimila.guerra
+ */
+public class TarefaService {
+    private List<Tarefas> tarefas = new arrayList<>();
+    private Long contadorId = 1L; //LITERAL
+
+    //Crir tarefas
+    public Tarefas criarTarefas(String titulo, String descricao) {
+        //Método construtor da classe tarefa
+        Tarefas tarefa = new Tarefas(contadorId++, titulo, descricao, false);
+        tarefas.add(tarefa);
+        return tarefa;
+    }
+    
+    //Listar todas as tarefas
+   public List<Tarefas> listarTarefas() {
+   return tarefas;
+   }
+    //git lab samuel novais
+
+    // Remover tarefa (Case 4)
+    public boolean removerTarefa(Long id) {
+        return tarefas.removeIf(t -> t.getId().equals(id));
+    }
+
+}
+
